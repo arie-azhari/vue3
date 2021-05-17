df7a4fb0b2120e61445bcdfa380b7d724f63981f
arie-azhari <ari@refactory.id>
2021-05-18 00:23:32 +0700

    bday

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>Home</div>
+  <div
+    class="
+      w-full
+      h-full
+      text-gray-300
+      bg-black
+      flex
+      justify-center
+      items-center
+    "
+  >
+    <div class="text-center">
+      <p class="text-2xl">Happy Birthday</p>
+      <p class="text-4xl">Almas Aqmarina</p>
+      <p class="mt-10">I Love You</p>
+    </div>
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
